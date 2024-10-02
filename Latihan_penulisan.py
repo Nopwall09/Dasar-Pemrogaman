d23e5fe459dca4cf81ef5618042ecc837de192ea
Naufal Athaya Khansa <109408823+hehe090904@users.noreply.github.com>
2024-10-02 10:41:45 +0700

    Pertemuan ke-3

---
+++ Latihan_penulisan.py
@@ -0,0 +1,32 @@
+#Aturan penulisan statement
+a = 5
+b =8
+c = a + b
+print (c)
+
+a = 5; b = 8; c = a + b #proses
+print(c) #hasil
+
+#Aturan penulisan statement
+print("D3 Manajemen Informatika") #output
+print("Mata Kuliah Dasar Pemrogaman") #output
+
+print("D3 Manajemen Informatika"); print("Mata Kuliah Dasar Pemrogamagn") #output dengan penggunaan ";" untuk sekat tiap perintah
+
+#Aturan Penulisan String
+kampus = "kampus Polinema PSDKU Kediri" #menggunakan petik 2
+prodi = 'D3 mamajemen informatika' #penggunaan petik 1
+mata_kuliah = """Dasar Pemrogamgan""" #penggunaan 3 petik 2
+praktik = '''praktikum Dasar Pemrogaman''' #penggunaan 3 petik 1
+
+print(kampus +"\n" + "\n" + mata_kuliah + "\n" + praktik) #penggunaan "\n" untuk mewakili karakter newline atau baris baru
+
+#Ini adalah contoh komentar
+#yang ditulis dengan
+#lebih satu baris
+print("komentar lebih satu baris") #output
+
+""""ini adalah contoh komentar
+    yang ditulis dengan
+    lebih satu baris"""
+print("komentar lebih satu baris") #ouput
---
+++ Perbaiki_kesalahan.py
@@ -0,0 +1,21 @@
+#Perbaiki identitas dan penulisan
+a = 10
+
+if a > 5:
+    print("Nilai a lebih besar dari 5") #penggunaan tab/blok dan penggunaan ("contoh") setelah perintah print
+    print("Pengecekan selesai") #penggunaan tab/blok dan penggunaan ("contoh") setelah perintah print
+else:
+    print("Nilai a tidak lebih besar dari 5") #penggunaan tab/blok dan penggunaan ("contoh") setelah perintah print
+    print("Pengecekan selesai") #penggunaan tab/blok dan penggunaan ("contoh") setelah perintah print
+
+#Perbaiki kesalahan penulisan
+"""Ini adalah 
+contoh aturan penulisan"""
+
+print("halo") #kurang petik 2 didalam ()
+print("saya seorang mahasiswa") #kurang kurung  buka dan tutup "()"
+#print("saya jurusan d3 mi")
+print("saya kuliah di Polinema") #tidak perlu ada In setelah print
+print("""saya ini semester 1""") #output
+
+
